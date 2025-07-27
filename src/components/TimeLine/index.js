ad8c1f6bd6c0c153a7bdf509b60663c4a08274ab
daniel <daniel@allenchow.com>
2025-07-26 22:22:18 -0600

    updated

--- src/components/TimeLine/index.js
+++ src/components/TimeLine/index.js
@@ -3,14 +3,11 @@ import { ReactComponent as WorkIcon } from './work.svg'
 import { ReactComponent as SchoolIcon } from './school.svg'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
-
 import timelineElements from './timelineElements'
-
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component'
-
 import 'react-vertical-timeline-component/style.min.css'
 
 const TimeLine = () => {
@@ -20,16 +17,15 @@ const TimeLine = () => {
     const timeoutId = setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
-
     return () => clearTimeout(timeoutId)
   }, [])
 
-  let workIconStyles = { background: '#06D6A0' }
-  let schoolIconStyles = { background: '#f9c74f' }
+  const workIconStyles = { background: '#06D6A0' }
+  const schoolIconStyles = { background: '#f9c74f' }
 
   return (
-    <div>
-      <h1>
+    <div className="timeline-container">
+      <h1 className="timeline-title">
         <AnimatedLetters
           letterClass={letterClass}
           strArray={['T', 'i', 'm', 'e', 'l', 'i', 'n', 'e']}
@@ -37,43 +33,48 @@ const TimeLine = () => {
         />
       </h1>
 
-      <VerticalTimeline>
-        {timelineElements.map((element) => {
-          let isWorkIcon = element.icon === 'work'
-          let showButton =
-            element.buttonText !== undefined &&
-            element.buttonText !== null &&
-            element.buttonText !== ''
+      <div className="timeline-wrapper">
+        <VerticalTimeline lineColor="#00bfa6">
+          {timelineElements.map((element) => {
+            const isWorkIcon = element.icon === 'work'
+            const showButton = element.buttonText?.trim()
 
-          return (
-            <VerticalTimelineElement
-              key={element.key}
-              date={element.date}
-              dateClassName="date"
-              iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
-              icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
-            >
-              <h3 className="vertical-timeline-element-title">
-                {element.title}
-              </h3>
-              <h5 className="vertical-timeline-element-subtitle">
-                {element.location}
-              </h5>
-              <p id="description">{element.description}</p>
-              {showButton && (
-                <a
-                  className={`button ${
-                    isWorkIcon ? 'workButton' : 'schoolButton'
-                  }`}
-                  href="https://github.com/danielclf6230"
-                >
-                  {element.buttonText}
-                </a>
-              )}
-            </VerticalTimelineElement>
-          )
-        })}
-      </VerticalTimeline>
+            return (
+              <VerticalTimelineElement
+                key={element.id}
+                date={element.date}
+                dateClassName="timeline-date"
+                iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
+                icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <ul className="description">
+                  {element.description.map((desc, idx) => (
+                    <li key={idx}>{desc}</li>
+                  ))}
+                </ul>
+                {showButton && (
+                  <a
+                    className={`button ${
+                      isWorkIcon ? 'workButton' : 'schoolButton'
+                    }`}
+                    href="https://github.com/danielclf6230"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {element.buttonText}
+                  </a>
+                )}
+              </VerticalTimelineElement>
+            )
+          })}
+        </VerticalTimeline>
+      </div>
     </div>
   )
 }
